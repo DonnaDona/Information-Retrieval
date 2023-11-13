b8fa254e8c9d58462a644b857eb328154f6cc847
Loredana De Filippo <defill@usi.ch>
2023-11-13 16:04:59 +0100

    WIP: connect the database

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -3,15 +3,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import os
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
 import psycopg2
 from dotenv import load_dotenv
 
 
 class ScraperPipeline:
+
     def __init__(self):
         load_dotenv()
 
@@ -28,72 +25,68 @@ class ScraperPipeline:
 
         ## Create quotes table if none exists
         self.cur.execute("""
-        CREATE TABLE IF NOT EXISTS Review(
-            id serial PRIMARY KEY, 
-            url text,,
-            score text,
-            title text
-            content text
-        )
-        """)
-
-        self.cur.execute("""
-            CREATE TABLE IF NOT EXISTS Movie(
+            CREATE TABLE IF NOT EXISTS reviews(
                 id serial PRIMARY KEY, 
                 url text,
-                image_url text,
+                score int,
                 title text,
-                description text
-                release text,
+                content text
+            )
+            """)
+
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS movies(
+                id serial PRIMARY KEY, 
+                title text,
+                description text,
+                release date,
                 duration text,
                 genres text,
                 score text,
                 director text,
                 actors text,
-                plot text,
-                metadata text,
+                metadata_url text,
+                metadata_image_url text,
+                metadata_page_title text
             )
             """)
 
+    def reviewItem(self, item):
+        self.cur.execute("""
+            INSERT INTO reviews (url, score, title, content) 
+            VALUES (%s, %s, %s, %s)
+        """, (
+            item["url"],
+            item["score"],
+            item["title"],
+            item["content"]
+        ))
+
+    def movieItem(self, item):
+        self.cur.execute("""
+            INSERT INTO movies (title, description, release, duration, genres, score, director, actors, metadata_url, metadata_image_url, metadata_page_title) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            item["title"],
+            item["description"],
+            item["release"],
+            item["duration"],
+            ','.join(item["genres"]),
+            item["score"],
+            item["director"],
+            ','.join(item["actors"]),
+            item["metadata"]["url"],
+            item["metadata"]["image_url"],
+            item["metadata"]["page_title"]
+        ))
+
     def process_item(self, item, spider):
-        # Check if the item already exists in the Review table
-        self.cur.execute("SELECT id FROM Review WHERE url = %s", (item["url"],))
-        existing_review = self.cur.fetchone()
 
-        if not existing_review:
-            # If the item does not exist, insert it into the Review table
-            self.cur.execute("""
-                        INSERT INTO Review (url, score, title, content) VALUES (%s, %s, %s, %s)
-                    """, (
-                item["url"],
-                item["score"],
-                item["title"],
-                item["content"]
-            ))
-
-        # Check if the item already exists in the Movie table
-        self.cur.execute("SELECT id FROM Movie WHERE url = %s", (item["url"],))
-        existing_movie = self.cur.fetchone()
-
-        if not existing_movie:
-            # If the item does not exist, insert it into the Movie table
-            self.cur.execute("""
-                        INSERT INTO Movie (url, image_url, title, description, release, duration, genres, score, director, actors, plot, metadata) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    """, (
-                item["url"],
-                item["image_url"],
-                item["title"],
-                item["description"],
-                item["release"],
-                item["duration"],
-                item["genres"],
-                item["score"],
-                item["director"],
-                item["actors"],
-                item["plot"],
-                item["metadata"]
-            ))
+        # check if item is a movie or a review
+        if "description" in item:
+            self.movieItem(item)
+        else:
+            self.reviewItem(item)
 
         # Execute insert of data into the database
         self.connection.commit()
