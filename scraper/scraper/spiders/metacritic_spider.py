a26da4e943638f22b64dab546c0ec5245cad6360
Loredana De Filippo <defill@usi.ch>
2023-12-05 10:39:36 +0100

    Update the metacritic_spider.

--- scraper/scraper/spiders/metacritic_spider.py
+++ scraper/scraper/spiders/metacritic_spider.py
@@ -9,36 +9,6 @@ from .utils import parse_duration, datestr_to_iso
 from ..items import Movie, Review
 
 
-def parse_movie_details(response: Response):
-    """
-    Parse the movie details, returning a tuple with the release date, the duration, the rating and the genres.
-
-    :param response: the response of the movie page
-    :return: a tuple with the release date, the duration, the rating and the genres
-    """
-    get_detail = lambda idx: response.css("div.c-movieDetails_sectionContainer")[idx].xpath("(./span)[2]/text()").get()
-
-    release = datestr_to_iso(get_detail(1))
-    duration = parse_duration(get_detail(2))
-    genres = response.css("div.c-movieDetails_sectionContainer")[4].xpath("./ul/li//span/text()").getall()
-    genres = list(map(str.strip, genres))
-
-    return release, duration, rating, genres
-
-
-def parse_movie(response: Response) -> Movie:
-    """
-    Parse a movie page, returning a `Movie` object. The `Movie` class is defined in `scraper/items.py`.
-
-    :param response: the response of the movie page
-    :return: a `Movie` object describing the movie
-    """
-    title: str = response.css("div.c-productHero_title").xpath("./div/text()").get().strip()
-    description: str = response.css("meta[name=description]::attr(content)").get().strip()
-    release, duration, rating, genres = parse_movie_details(response)
-    return Movie(title=title, description=description, release=release, duration=duration, rating=rating, genres=genres)
-
-
 def parse_reviews(response: Response):
     """
     Parse the reviews page, yielding a `Review` for each review in the page.
@@ -80,6 +50,16 @@ class MetacriticSpider(scrapy.Spider):
         """
         return f"{self.DOMAIN}/movie/{movie_slug}/user-reviews"
 
+    def get_movie_id(self, url: str) -> str:
+        """
+        Given a movie URL, return the movie id.
+        For example, given https://www.metacritic.com/movie/the-godfather/, return "the-godfather".
+
+        :param url: the URL of the movie
+        :return: the movie id
+        """
+        return url.split("/")[4]
+
     def next_page(self, response: Response) -> Request:
         """
         Given a browse page, return a `Request` for the next page.
@@ -87,14 +67,15 @@ class MetacriticSpider(scrapy.Spider):
         parsed_url = urlparse(response.url)
         query_params = parse_qs(parsed_url.query)
         current_page = int(query_params["page"][0])
-        return response.follow(f"{self.BASE_BROWSE_URL}?page={current_page + 1}", callback=self.parse_browse_page)
+        return response.follow(f"{self.BASE_BROWSE_URL}?page={current_page + 1}", callback=self.parse_browse_page,
+                               priority=0)
 
     def parse_browse_page(self, response: Response) -> Generator[Request, None, None]:
         """
         Parse a browse page, yielding a `Request` for each movie in the page and a `Request` for the next page.
         """
         for movie_url in response.xpath("//div[@class='c-productListings']//a/@href").extract():
-            yield response.follow(f"{self.DOMAIN}{movie_url}", callback=parse_movie)
+            yield response.follow(f"{self.DOMAIN}{movie_url}", callback=self.parse_movie, priority=1)
         yield self.next_page(response)
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
@@ -108,6 +89,56 @@ class MetacriticSpider(scrapy.Spider):
         page_type = page_url.split("/")[3]
 
         if page_type == "movie":
-            yield parse_movie(response)
+            yield self.parse_movie(response)
         elif page_type == "browse":
             yield from self.parse_browse_page(response)
+
+    def parse_metadata(self, response: Response) -> Movie.Metadata:
+        url = response.xpath("//link[@rel='canonical']/@href").get()
+        image_url = response.xpath("//meta[@property='og:image']/@content").get()
+        page_title = response.xpath("//title/text()").get()
+        return Movie.Metadata(url=url, image_url=image_url, page_title=page_title)
+
+    def parse_movie(self, response: Response):
+        """
+        Parse the movie details, returning a tuple with the release date, the duration, the rating and the genres.
+
+        :param response: the response of the movie page
+        :return: a tuple with the release date, the duration, the rating and the genres
+        """
+
+        movie_id = self.get_movie_id(response.url)
+        title = response.css("div.c-productHero_title").xpath("./div/text()").get().strip()
+
+        description = response.xpath("//meta[@name='description']/@content").get()
+        plot = description
+
+        release_date_str = response.xpath(
+            "//div[contains(@class, 'c-movieDetails')]//div[contains(span, 'Release Date')]/span[2]/text()") \
+            .get()
+        release_date = datestr_to_iso(release_date_str, "%b %d, %Y")
+
+        duration_str = response.xpath("//div[contains(@class, 'c-heroMetadata')]//li[4]/span/text()").get().strip()
+        duration = parse_duration(duration_str)
+
+        genres = response.xpath("//ul[contains(@class, 'c-genreList')]//span/text()")
+        genres = list(set(genres.get().strip() for genres in genres))
+
+        critic_score = float(response.xpath(
+            "//div[contains(@class, 'c-siteReviewScore_background-critic_medium')]//span/text()").get()) / 10 \
+            or float('nan')
+
+        user_score = float(
+            response.xpath("//div[contains(@class, 'c-siteReviewScore_background-user')]//span/text()").get() or float(
+                'nan'))
+
+        director = response.xpath("//div[contains(@class, 'c-productDetails_staff_directors')]//a/text()").get().strip()
+
+        actors = response.xpath("//div[contains(@data-cy, 'cast-')]//h3/text()")
+        actors = list(set(actors.get().strip() for actors in actors))
+
+        metadata = self.parse_metadata(response)
+
+        yield Movie(movie_id=movie_id, title=title, description=description, release=release_date, duration=duration,
+                    genres=genres, score=user_score, critic_score=critic_score, director=director, actors=actors,
+                    metadata=metadata, plot=plot)
