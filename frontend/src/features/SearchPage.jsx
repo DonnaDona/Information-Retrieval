34b9996e9e478bdc733685cffee722966c00d2e8
Loredana De Filippo <defill@usi.ch>
2023-12-11 20:34:30 +0100

    WIP: Add background image.

--- frontend/src/features/SearchPage.jsx
+++ frontend/src/features/SearchPage.jsx
@@ -5,7 +5,7 @@ import {SearchCard} from "../components/SearchCard.jsx";
 export function SearchPage() {
 
     return (
-        <Stack padding={20} alignItems="center">
+        <Stack className="search-page-container" padding={20} alignItems="center">
             <img src='../../public/robotLogo.svg' alt="logo" width="200" height="200"/>
             <Typography variant="h3" component="div" sx={{paddingBottom: 4}}>
                 Search & Chill
